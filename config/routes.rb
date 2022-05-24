4013cf7ae8a1a493daeacd13f9b0a13ea4e1ed11
nwabor <knwabor@gmail.com>
2022-05-24 21:21:33 +0100

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
