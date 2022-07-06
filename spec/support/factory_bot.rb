3d9979696ecffb2594774463890b4cf3cac24023
unknown <akinwun@gmail.com>
2022-07-06 10:02:31 +0100

    rspec issue resolved

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-RSpec.configure do |config|
-    config.include FactoryBot::Syntax::Methods
-end
-=======
 require 'rails_helper'
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
-<<<<<<< HEAD
->>>>>>> 922303d (gemfile unstashed)
-=======
->>>>>>> b2e3102 (rubocop issue fixed)
+
