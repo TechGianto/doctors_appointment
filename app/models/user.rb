271e85adcc7d26219a6bf59aafa5115ef6617a8e
standardgit <standardblaise@gmail.com>
2022-07-14 12:12:24 +0100

    Validation status for omnoauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  enum status: {active: 1, inactive: 2, banned: 3}
-  validates :last_name, :first_name, :email, :password, :phone, :status, :gender, presence: true
-  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, length: {minimum: 5}
-  validates :phone, length: {minimum: 11, maximum: 15}, format: {with: /\A[+-]?\d+\z/}
-  validates :profile_pic, :state, :gender, :middle_name, :nationality, :LGA, :status, presence: true, allow_nil: true
-  validates :gender, inclusion: {in: ['Male', 'Female']}
+         :recoverable, :rememberable, :validatable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google, :facebook]
+  enum status: {active: 1, inactive: 2, banned: 3}
+  validates :last_name, :first_name, :email, :password, :phone, :status, :gender, presence: true, allow_nil: true
+  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :password, length: {minimum: 5}
+  validates :phone, length: {minimum: 11, maximum: 15}, format: {with: /\A[+-]?\d+\z/}, allow_nil: true
+  validates :profile_pic, :state, :gender, :middle_name, :nationality, :LGA, :status, presence: true, allow_nil: true
+  #validates :gender, inclusion: {in: ['Male', 'Female']}, allow_nil: false
+         
 
   def self.from_omniauth(access_token)
     data = access_token.info
