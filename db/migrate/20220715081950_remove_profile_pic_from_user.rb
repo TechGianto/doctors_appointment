75c46c5c232ed2764f28b8b7a6bc27f3b9281e95
Isaiah peter <peterisaiah4fun@gmail.com>
2022-07-18 15:43:01 +0100

    Add down function to revert to old changes

--- db/migrate/20220715081950_remove_profile_pic_from_user.rb
+++ db/migrate/20220715081950_remove_profile_pic_from_user.rb
@@ -1,5 +1,15 @@
 class RemoveProfilePicFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column(:users, :profile_pic, :text)
+  def change 
+    def up
+      if column_exists?(:doctors, :certificate)
+        remove_column(:users, :profile_pic, :text)
+       end
+     end
+     
+     def down
+      if !column_exists?(:doctors, :certificate)
+        add_column :users, :profile_pic, :text
+       end
+     end
   end
 end
