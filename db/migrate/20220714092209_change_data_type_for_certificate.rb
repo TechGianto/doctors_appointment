3b80bea003f606eb320a2be370d85f11c5dde5cf
unknown <akinwun@gmail.com>
2022-07-18 15:45:54 +0100

    certificate column remove_column code refactored

--- db/migrate/20220714092209_change_data_type_for_certificate.rb
+++ db/migrate/20220714092209_change_data_type_for_certificate.rb
@@ -1,3 +1,15 @@
 class ChangeDataTypeForCertificate < ActiveRecord::Migration[7.0]
-  remove_column(:doctors, :certificate)
+  def change
+    def up
+      if column_exists?(:doctors, :certificate)
+        remove_column(:doctors, :certificate)
+       end
+     end
+     
+     def down
+      if !column_exists?(:doctors, :certificate)
+       add_column :doctors, :certificate, :string
+       end
+     end
+  end
 end
