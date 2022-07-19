523b53721f59cc80da407c758c4b9f00c658adcf
Isaiah peter <peterisaiah4fun@gmail.com>
2022-07-19 09:55:52 +0100

    change the ruby version

--- db/migrate/20220715081950_remove_profile_pic_from_user.rb
+++ db/migrate/20220715081950_remove_profile_pic_from_user.rb
@@ -1,13 +1,13 @@
 class RemoveProfilePicFromUser < ActiveRecord::Migration[7.0]
   def change 
     def up
-      if column_exists?(:doctors, :certificate)
+      if column_exists?(:users, :profile_pic)
         remove_column(:users, :profile_pic, :text)
        end
      end
      
      def down
-      if !column_exists?(:doctors, :certificate)
+      if !column_exists?(:users, :profile_pic)
         add_column :users, :profile_pic, :text
        end
      end
