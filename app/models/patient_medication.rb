96bb260ae936c4554691f4452b8995b006597680
standardgit <standardblaise@gmail.com>
2022-07-22 13:40:42 +0100

    Creating medications table

--- app/models/patient_medication.rb
+++ app/models/patient_medication.rb
@@ -3,13 +3,11 @@ class PatientMedication < ApplicationRecord
   belongs_to :doctor
 
   enum status: { started: 1, in_progress: 2, completed: 3, cancelled: 4 }
-  validates :medication_name, :dose, :dosage, :no_of_days, :user_id, :doctor_id, presence: true
-  
-  
+  validates :medication_name, :dose, :dosage, :no_of_days, :user_id, :doctor_id
 
   def self.patient_medication_for_today(user)
     user.patient_medications.filter do |item|
-      item.created_at + item.no_of_days.days >= Time.now
+      item.created_at + item.no_of_days.days >= Time.current
     end
   end
 end
