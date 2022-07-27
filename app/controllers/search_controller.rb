8d41f12f84b1378b9523b918716b7530d33a8787
nwabor <knwabor@gmail.com>
2022-07-27 07:36:21 +0100

    Added changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
   private
 
         def index
-            if !user_signed_in?
+            if user_signed_in?
                 redirect_to new_user_session_path
             end
         end
