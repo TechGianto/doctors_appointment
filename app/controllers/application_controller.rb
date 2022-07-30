aacb09fbdaee46f44ca34816ca74a4c6df3b5751
unknown <akinwun@gmail.com>
2022-07-30 18:51:18 +0100

    search sidebar filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-    def require_login
-        if !user_signed_in?
-            redirect_to new_user_session_path
+        def require_login
+            if user_signed_in?
+                redirect_to new_user_session_path
+            end
         end
     end
 
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
     before_action :require_login, only: [:index]
 
-    def index; end
+    def index
+      @speciality = Speciality.first(10)
+      @user_state = User.select(:state).distinct.order(state: :asc)
+      @user_region = User.select(:LGA).distinct.order(LGA: :asc)
+      @user_qualification = Qualification.select(:name).order(name: :asc)
+    end
+
 end
