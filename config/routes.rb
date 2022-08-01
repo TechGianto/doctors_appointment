e9ccf001dbe0fba9f0207ad23c61050169e0ece0
standardgit <standardblaise@gmail.com>
2022-08-01 16:27:54 +0100

    Patient header and sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  #get 'patient/patient'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   root 'home#index'
+<<<<<<< HEAD
   controller :home do
     get :services, action: :services
     get :hello, action: :test
@@ -17,5 +19,9 @@ Rails.application.routes.draw do
   # resources :doctor, only: [:index, :show] do
   #   post :sack
   # end
+=======
+  get 'home/services'
+  resources :patient
+>>>>>>> 5c8dae6 (Patient header and sidebar)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
