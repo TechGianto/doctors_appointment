b8a421c8e15899a9593d170f2943d8e1f350ca29
unknown <akinwun@gmail.com>
2022-08-02 10:16:50 +0100

    search sidebar filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-        def require_login
-            if user_signed_in?
-                redirect_to new_user_session_path
-            end
+    def require_login
+        if !user_signed_in?
+            redirect_to new_user_session_path
         end
     end
 
-  def user
-    current_user
-  end
+    def user
+      current_user
+    end
 end
