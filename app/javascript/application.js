9e712a5a514be57d1e3e2bc81fd2696c0fad1fe5
unknown <akinwun@gmail.com>
2022-08-03 11:05:10 +0100

    search sidebar filter

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -35,11 +35,32 @@ $(document).ready(function(){
 
     $('.filter-icon').click(function(){
       $('#sidebar-toggle').removeClass("col-3")
-      document.getElementById('search-sidebar').style.visibility = 'hidden'
+      $('#search-main').removeClass("col-9")
+      $('#search-main').addClass("col-11")
+      document.getElementById('search-sidebar-form').style.visibility = 'hidden'
       document.getElementById('search-filter').style.background = 'transparent'
       $('#sidebar-toggle').addClass("col-1")
       $('#filter-wrap').addClass('filter-wrap-toggle')
       $('#filter-text').removeClass("filter-text")
       $('#filter-text').addClass("filter-text-toggle")
+      $('.filter-icon').hide()
+      $('#filter-icon-container').addClass('filter-icon-container')
+      $('.filter-icon-expand').show()
     });
+    
+    $('.filter-icon-expand').click(function(){
+      $('#sidebar-toggle').removeClass("col-1")
+      $('#sidebar-toggle').addClass("col-3")
+      document.getElementById('search-sidebar-form').style.visibility = 'visible'
+      document.getElementById('search-filter').style.background = '#fcffef'
+      $('#filter-wrap').removeClass("filter-wrap-toggle")
+      $('#filter-wrap').addClass('filter-wrap')
+      $('#filter-text').removeClass("filter-text-toggle")
+      $('#filter-text').addClass("filter-text")
+      $('#search-main').removeClass("col-11")
+      $('#search-main').addClass("col-9")
+      $('.filter-icon-expand').hide()
+      $('.filter-icon').show()
+    })
+
   });
