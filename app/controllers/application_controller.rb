ca36d3730e5621043f78753ec8681905d3c17c62
AkinAgbejoye <79208021+AkinAgbejoye@users.noreply.github.com>
2022-08-10 11:18:22 +0100

    Akin/ch116/search form and sidebar on doctors page (#50)
    * search sidebar filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
         end
     end
 
-  def user
-    current_user
-  end
+    def user
+      current_user
+    end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
     before_action :require_login, only: [:index]
 
-    def index; end
+    def index
+      @specialities = Speciality.first(10)
+      @user_states = User.select(:state).distinct.order(state: :asc)
+      @user_regions = User.select(:LGA).distinct.order(LGA: :asc)
+      @user_qualifications = Qualification.select(:name).order(name: :asc)
+    end
+
 end
