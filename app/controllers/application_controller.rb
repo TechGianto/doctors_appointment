3b99936ff45fb23361cbc43567426a644df8c97f
Rails <60232994+RailsAbam@users.noreply.github.com>
2022-08-14 22:13:52 +0100

    Login signup request passwrd (#43)
    * styles the login form and create form add svg img

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, if: :devise_controller?
 
   private
 
@@ -10,5 +11,9 @@ class ApplicationController < ActionController::Base
 
     def user
       current_user
+    end
+
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
     end
 end
