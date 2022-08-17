ee974404554cb3c3715cd8f9f68f64bad64bb328
unknown <akinwun@gmail.com>
2022-08-17 20:24:22 +0100

    sidebar changed to react

---
+++ app/javascript/bundles/DoctorSearchSidebar/reducers/SideBarReducer.js
@@ -0,0 +1,41 @@
+import { combineReducers } from 'redux';
+import { STATES, REGIONS, SPECIALITIES, QUALIFICATIONS } from '../constants/SideBarConstants';
+
+const regions = (state = '', action) => {
+  switch (action.type) {
+    case REGIONS:
+      return action.text;
+    default:
+      return state;
+  }
+};
+
+const states = (state = '', action) => {
+  switch (action.type) {
+    case STATES:
+      return action.text;
+    default:
+      return state;
+  }
+};
+
+const specialities = (state = '', action) => {
+  switch (action.type) {
+    case SPECIALITIES:
+      return action.text;
+    default:
+      return state;
+  }
+};
+
+const qualifications = (state = '', action) => {
+  switch (action.type) {
+    case QUALIFICATIONS:
+      return action.text;
+    default:
+      return state;
+  }
+};
+const SideBarReducer = combineReducers({ regions, states, specialities, qualifications });
+
+export default SideBarReducer;
