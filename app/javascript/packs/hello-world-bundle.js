5a0ad5920b19482063744aeaea6e863054c636fd
standardgit <standardblaise@gmail.com>
2022-08-18 23:03:39 +0100

    patient sidebar to component

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,10 +1,14 @@
 import ReactOnRails from "react-on-rails";
 
 import HelloWorldApp from "../bundles/HelloWorld/startup/HelloWorldApp";
+import PatientHeader from "../bundles/Patient/component/PatientHeader";
+import Overview from "../bundles/Patient/component/Overview";
+import PatientSideBar from "../bundles/Patient/component/PatientSidebar";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorldApp,
   PatientHeader,
+  PatientSideBar,
   Overview,
 });
