32decb859fdb7a5981f8ff1ea3b694b324cd5cdd
unknown <akinwun@gmail.com>
2022-08-18 04:40:37 +0100

    sidebar changed to react

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
     before_action :require_login, only: [:index]
-    layout "side_bar", only: [:index]
+    layout 'side_bar', only: [:index]
     # def index
     #   @specialities = Speciality.first(10)
     #   @user_states = User.select(:state).distinct.order(state: :asc)
@@ -9,7 +9,7 @@ class SearchController < ApplicationController
     # end
 
     def index
-      @side_bar_props = {regions: User.select(:LGA).distinct.order(LGA: :asc),  states: User.select(:state).distinct.order(state: :asc), specialities:Speciality.first(10), qualifications: Qualification.select(:name).order(name: :asc) }
+      @side_bar_props = {regions: User.select(:LGA).distinct.order(LGA: :asc), states: User.select(:state).distinct.order(state: :asc), specialities: Speciality.first(10), qualifications: Qualification.select(:name).order(name: :asc) }
     end
 
 end
