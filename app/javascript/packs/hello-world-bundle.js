8348ed8f414e4385b60c067428e0583e488cf37c
Isaiah peter <peterisaiah4fun@gmail.com>
2022-08-18 11:37:17 +0100

    change the top bar to react component

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,8 +1,10 @@
-import ReactOnRails from 'react-on-rails';
+import ReactOnRails from "react-on-rails";
 
-import HelloWorldApp from '../bundles/HelloWorld/startup/HelloWorldApp';
+import HelloWorldApp from "../bundles/HelloWorld/startup/HelloWorldApp";
+import PatientHeader from "../bundles/Patient/component/PatientHeader";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorldApp,
+  PatientHeader,
 });
