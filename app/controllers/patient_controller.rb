72282fe5a2835c249aab2e6425954669c545c196
Isaiah peter <peterisaiah4fun@gmail.com>
2022-08-22 08:45:56 +0100

    fix failing test

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -3,6 +3,11 @@ class PatientController < ApplicationController
   layout 'patient'
 
   def index
-    @patient_header_props = {profile_pic: url_for(current_user.profile_pic).to_s, first_name: current_user.first_name.to_s, roles: current_user.roles[0].name.to_s, signed_in: user_signed_in?, id: current_user.id}
+    if current_user.profile_pic.attached?
+      picture = url_for(current_user.profile_pic).to_s
+    else
+      picture = 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg'
+    end
+    @patient_header_props = {profile_pic: picture, first_name: current_user.first_name.to_s, roles: current_user.roles[0].name.to_s, signed_in: user_signed_in?, id: current_user.id}
   end
 end
