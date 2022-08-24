97941f468238e0611a923b644ea32a6ea06fb22c
Nwabor Kelvin <knwabor@gmail.com>
2022-08-24 01:21:16 +0100

    React Redux

--- app/javascript/bundles/DoctorSearchSidebar/containers/SideBarContainer.js
+++ app/javascript/bundles/DoctorSearchSidebar/containers/SideBarContainer.js
@@ -1,13 +1,19 @@
 // Simple example of a React "smart" component
 
-import { connect } from 'react-redux';
-import SideBar from '../components/SideBar';
-import * as actions from '../actions/SideBarActionCreators';
+import { connect } from "react-redux";
+import * as actions from "../actions/SideBarActionCreators";
+import DoctorsSearchPage from "../components/DoctorsSearchPage";
+import SideBar from "../components/SideBar";
 
 // Which part of the Redux global state does our component want to receive as props?
-const mapStateToProps = (state) => ({ regions: state.regions, states: state.states, specialities:state.specialities, qualifications: state.qualifications});
+const mapStateToProps = (state) => ({
+  regions: state.regions,
+  states: state.states,
+  specialities: state.specialities,
+  qualifications: state.qualifications,
+});
 
 // Don't forget to actually use connect!
 // Note that we don't export HelloWorld, but the redux "connected" version of it.
 // See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
-export default connect(mapStateToProps, actions)(SideBar);
+export default connect(mapStateToProps, actions)(DoctorsSearchPage);
