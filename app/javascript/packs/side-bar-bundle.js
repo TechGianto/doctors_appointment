9783ca4f9835a4a9caa38d6550fafeb303ed2ed0
Jude Etanuwoma <jude.etanuwoma@andela.com>
2022-08-24 11:13:49 +0100

    Remove unused bundle

--- app/javascript/packs/side-bar-bundle.js
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import SideBarApp from '../bundles/DoctorSearchSidebar/startup/SideBarApp';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  SideBarApp,
-});
