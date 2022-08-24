1e373a867f27a34f812bd2a8b0eba8ae9d4612e6
Nwabor Kelvin <knwabor@gmail.com>
2022-08-24 01:21:16 +0100

    React Redux

--- app/javascript/bundles/DoctorSearchSidebar/startup/SideBarApp.jsx
+++ app/javascript/bundles/DoctorSearchSidebar/startup/SideBarApp.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-
-import configureStore from '../store/SideBarStore';
-import SideBarContainer from '../containers/SideBarContainer';
+import React from "react";
+import { Provider } from "react-redux";
+import configureStore from "../store/SideBarStore";
+import SideBarContainer from "../containers/SideBarContainer";
 
 // See documentation for https://github.com/reactjs/react-redux.
 // This is how you get props from the Rails view into the redux store.
