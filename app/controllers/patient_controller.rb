544bb8c77119d9650770393dff5df099f4282692
Isaiah peter <peterisaiah4fun@gmail.com>
2022-08-25 13:37:47 +0100

    add default image

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -4,12 +4,12 @@ class PatientController < ApplicationController
 
   def index
     speciality = Speciality.all
-    location = User.with_role(:Doctor).pluck(:state)
+    location = User.with_role(:Doctor).pluck(:state, :id)
 
     if current_user.profile_pic.attached?
       picture = url_for(current_user.profile_pic).to_s
     else
-      picture = 'https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg'
+      picture = ''
     end
     @patient_header_props = {profile_pic: picture, first_name: current_user.first_name.to_s, roles: current_user.roles[0].name.to_s, signed_in: user_signed_in?, id: current_user.id, speciality: speciality, location: location}
   end
