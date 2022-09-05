ff30572ea6b65a33ba01aea5356bcf84ba066b3b
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-05 21:48:11 +0100

    fix a react lint failing error for all react component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,61 +6,60 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-$(document).ready(function(){
-    $('.doctor-slider').slick({
-      centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      responsive: [
+$(document).ready(function () {
+  $(".doctor-slider").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 2
-        }
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 601,
         settings: {
           arrows: false,
           centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-          }
-        }
-      ]
-    });;
-
-    $('.filter-icon').click(function(){
-      $('#sidebar-toggle').removeClass("col-3")
-      $('#search-main').removeClass("col-9")
-      $('#search-main').addClass("col-11")
-      document.getElementById('search-sidebar-form').style.visibility = 'hidden'
-      document.getElementById('search-filter').style.background = 'transparent'
-      $('#sidebar-toggle').addClass("col-1")
-      $('#filter-wrap').addClass('filter-wrap-toggle')
-      $('#filter-text').removeClass("filter-text")
-      $('#filter-text').addClass("filter-text-toggle")
-      $('.filter-icon').hide()
-      $('#filter-icon-container').addClass('filter-icon-container')
-      $('.filter-icon-expand').show()
-    });
-    
-    $('.filter-icon-expand').click(function(){
-      $('#sidebar-toggle').removeClass("col-1")
-      $('#sidebar-toggle').addClass("col-3")
-      document.getElementById('search-sidebar-form').style.visibility = 'visible'
-      document.getElementById('search-filter').style.background = '#fcffef'
-      $('#filter-wrap').removeClass("filter-wrap-toggle")
-      $('#filter-wrap').addClass('filter-wrap')
-      $('#filter-text').removeClass("filter-text-toggle")
-      $('#filter-text').addClass("filter-text")
-      $('#search-main').removeClass("col-11")
-      $('#search-main').addClass("col-9")
-      $('.filter-icon-expand').hide()
-      $('.filter-icon').show()
-    })
-
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
+
+  $(".filter-icon").click(function () {
+    $("#sidebar-toggle").removeClass("col-3");
+    $("#search-main").removeClass("col-9");
+    $("#search-main").addClass("col-11");
+    document.getElementById("search-sidebar-form").style.visibility = "hidden";
+    document.getElementById("search-filter").style.background = "transparent";
+    $("#sidebar-toggle").addClass("col-1");
+    $("#filter-wrap").addClass("filter-wrap-toggle");
+    $("#filter-text").removeClass("filter-text");
+    $("#filter-text").addClass("filter-text-toggle");
+    $(".filter-icon").hide();
+    $("#filter-icon-container").addClass("filter-icon-container");
+    $(".filter-icon-expand").show();
+  });
+
+  $(".filter-icon-expand").click(function () {
+    $("#sidebar-toggle").removeClass("col-1");
+    $("#sidebar-toggle").addClass("col-3");
+    document.getElementById("search-sidebar-form").style.visibility = "visible";
+    document.getElementById("search-filter").style.background = "#fcffef";
+    $("#filter-wrap").removeClass("filter-wrap-toggle");
+    $("#filter-wrap").addClass("filter-wrap");
+    $("#filter-text").removeClass("filter-text-toggle");
+    $("#filter-text").addClass("filter-text");
+    $("#search-main").removeClass("col-11");
+    $("#search-main").addClass("col-9");
+    $(".filter-icon-expand").hide();
+    $(".filter-icon").show();
+  });
+});
