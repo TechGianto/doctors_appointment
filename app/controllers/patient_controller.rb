4c2af44fb69f33a628aebe0e71d48687fe514636
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-14 09:59:25 +0100

    change how location and speciality fetch data using memorization

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -3,14 +3,14 @@ class PatientController < ApplicationController
   layout 'patient'
 
   def index
-    speciality = Speciality.all
-    location = User.with_role(:Doctor).pluck(:state, :id)
+    @speciality ||= Speciality.all
+    @location ||= User.with_role(:Doctor).pluck(:state, :id)
 
     if current_user.profile_pic.attached?
       picture = url_for(current_user.profile_pic).to_s
     else
       picture = ''
     end
-    @patient_header_props = {profile_pic: picture, first_name: current_user.first_name.to_s, roles: current_user.roles[0].name.to_s, signed_in: user_signed_in?, id: current_user.id, speciality: speciality, location: location}
+    @patient_header_props = {profile_pic: picture, first_name: current_user.first_name.to_s, roles: current_user.roles[0].name.to_s, signed_in: user_signed_in?, id: current_user.id, speciality: @speciality, location: @location}
   end
 end
