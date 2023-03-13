39bd40ce6389ec0111cbaf82427d6b5024e608af
standardgit <standardblaise@gmail.com>
2023-03-13 09:20:06 +0100

    Overview design page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_22_185317) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_080430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -152,6 +152,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_185317) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_vitals", force: :cascade do |t|
+    t.string "blood_pressure", default: "N/A"
+    t.float "weight", default: 0.0
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_vitals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -172,6 +181,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_185317) do
     t.string "nationality"
     t.string "LGA"
     t.integer "status", default: 1
+    t.integer "age"
+    t.string "blood_group"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -199,4 +210,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_185317) do
   add_foreign_key "doctors_qualifications", "qualifications"
   add_foreign_key "patient_medications", "doctors"
   add_foreign_key "patient_medications", "users"
+  add_foreign_key "user_vitals", "users"
 end
